dd167d5dda7d14d1f4bcb6adb91c2290f9ca0891
Ayush Jain <ayushjain2205@gmail.com>
2023-02-23 11:04:13 +0530

    Configure tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,30 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        goldenrod: "#ffc022",
+        black: "#000",
+        gray: {
+          100: "#262626",
+          200: "rgba(0, 0, 0, 0.85)",
+          300: "rgba(0, 0, 0, 0.54)",
+          400: "rgba(0, 0, 0, 0.58)",
+        },
+        orangered: "#f24e1e",
+        white: "#fff",
+        mediumblue: "#3a00e5",
+      },
+      fontFamily: { "work-sans": "'Work Sans'" },
+    },
+    fontSize: {
+      base: "12px",
+      lg: "14px",
+      xl: "16px",
+      "2xl": "20px",
+      "3xl": "24px",
+      "4xl": "32px",
+    },
   },
   plugins: [],
 }
