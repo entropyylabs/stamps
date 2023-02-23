62df7b9b4cb6b1e3904d8b59eb0bcfa86572991c
Ayush Jain <ayushjain2205@gmail.com>
2023-02-23 11:38:48 +0530

    Add mobile only support

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -1,8 +1,5 @@
-import { Box, Flex } from "@chakra-ui/layout"
 import { useDisclosure, useMediaQuery } from "@chakra-ui/react"
-
-import { Footer } from "../ui/Footer"
-import { Navbar } from "../ui/Navbar/Nav"
+import { useState, useEffect } from "react"
 type Props = {
   children: React.ReactNode
   showSidebar?: boolean
@@ -10,25 +7,10 @@ type Props = {
 }
 
 export default function AppLayout({ children, showSidebar = false }: Props) {
-  const { isOpen, onOpen } = useDisclosure()
+  const [isMobile] = useMediaQuery("(max-width: 768px)")
 
-
-  return (
-    <>
-      <Flex direction="column" minH="90vh" w="full">
-        <Navbar onOpen={onOpen} />
-        <Flex bg="page.background" flexGrow={1}>
-          <Box
-            w="100%"
-            p={showSidebar ? "1rem" : 0}
-            paddingTop={{ base: "60px", md: "90px" }}
-            position="relative"
-          >
-            {children}
-          </Box>
-        </Flex>
-      </Flex>
-      <Footer />
-    </>
-  )
+  if (isMobile) {
+    return <>{children}</>
+  }
+  return <div>Please open this link on a mobile device for the complete experience!</div>
 }
