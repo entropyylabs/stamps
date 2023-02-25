a3bda930939c6832043cb9ebdb1e39b9d11d1a07
Ayush Jain <ayushjain2205@gmail.com>
2023-02-25 23:15:22 +0530

    Update Camera view

--- src/components/Cameraview.js
+++ src/components/Cameraview.js
@@ -1,11 +1,29 @@
-import React, { useState, useRef } from "react"
+import React, { useRef } from "react"
 import { Camera } from "react-camera-pro"
 
 const Cameraview = () => {
   const camera = useRef(null)
+  const errorMessages = {
+    noCameraAccessible:
+      "No camera device accessible. Please connect your camera or try a different browser.",
+    permissionDenied: "Permission denied. Please refresh and give camera permission.",
+    switchCamera:
+      "It is not possible to switch camera to different one because there is only one video device accessible.",
+    canvas: "Canvas is not supported.",
+  }
+
+  const videoConstraints = {
+    facingMode: { exact: "environment" },
+  }
+
   return (
-    <div>
-      <Camera ref={camera} facingMode="environment" style={{ zIndex: 0 }} />
+    <div style={{ height: "700px" }}>
+      <Camera
+        errorMessages={errorMessages}
+        ref={camera}
+        facingMode="environment"
+        style={{ zIndex: 0, height: "695px" }}
+      />
     </div>
   )
 }
